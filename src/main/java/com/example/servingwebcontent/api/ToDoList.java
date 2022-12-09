31f4e6ed9701a8d8a03ffffc17603550473ff1ef
MaximSchmidt <Maxim.Schmidt@student.htw-berlin.de>
2022-12-09 22:09:44 +0100

    ToDo neue Attribute + Manipulation Request neu + Dependency validation

--- src/main/java/com/example/servingwebcontent/api/ToDoList.java
+++ src/main/java/com/example/servingwebcontent/api/ToDoList.java
@@ -1,15 +1,25 @@
 package com.example.servingwebcontent.api;
 
+import java.time.LocalDate;
+
 public class ToDoList {
 
     private long id;
+    private String titel;
     private String description;
-    private boolean complete;
+    private String category;
+    private LocalDate date;
+    private Boolean complete;
+    private Boolean priority;
 
-    public ToDoList(long id, String description, boolean complete) {
+    public ToDoList(long id, String titel, String description, String category, LocalDate date, Boolean complete, Boolean priority) {
         this.id = id;
+        this.titel = titel;
         this.description = description;
+        this.category = category;
+        this.date = date;
         this.complete = complete;
+        this.priority = priority;
     }
 
     public long getId() {
@@ -20,6 +30,14 @@ public class ToDoList {
         this.id = id;
     }
 
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
     public String getDescription() {
         return description;
     }
@@ -28,14 +46,35 @@ public class ToDoList {
         this.description = description;
     }
 
-    public boolean isComplete() {
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Boolean getComplete() {
         return complete;
     }
 
-    public void setComplete(boolean complete) {
+    public void setComplete(Boolean complete) {
         this.complete = complete;
     }
 
+    public Boolean getPriority() {
+        return priority;
+    }
 
-
+    public void setPriority(Boolean priority) {
+        this.priority = priority;
+    }
 }
--- src/main/java/com/example/servingwebcontent/api/ToDoListManipulationRequest.java
+++ src/main/java/com/example/servingwebcontent/api/ToDoListManipulationRequest.java
@@ -1,13 +1,39 @@
 package com.example.servingwebcontent.api;
 
+import java.time.LocalDate;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
+
 public class ToDoListManipulationRequest {
 
+    @Size(min = 1, message = "Beschreibe deinen Titel mit mindestens 1 Buchstaben")
+    private String titel;
+    @NotBlank(message = "Beschreibe deine Aufgabe")
     private String description;
-    private boolean complete;
+    @NotBlank(message = "Wähle eine Kategorie")
+    private String category;
+    @FutureOrPresent(message = "Wähle eine Deadline für deine Aufgabe")
+    private LocalDate date;
+    private Boolean complete;
+    private Boolean priority;
 
-    public ToDoListManipulationRequest(String description, boolean complete) {
+    public ToDoListManipulationRequest(String titel, String description, String category, LocalDate date, Boolean complete, Boolean priority) {
+        this.titel = titel;
         this.description = description;
+        this.category = category;
+        this.date = date;
         this.complete = complete;
+        this.priority = priority;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
     }
 
     public String getDescription() {
@@ -18,13 +44,35 @@ public class ToDoListManipulationRequest {
         this.description = description;
     }
 
-    public boolean isComplete() {
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Boolean getComplete() {
         return complete;
     }
 
-    public void setComplete(boolean complete) {
+    public void setComplete(Boolean complete) {
         this.complete = complete;
     }
 
+    public Boolean getPriority() {
+        return priority;
+    }
 
+    public void setPriority(Boolean priority) {
+        this.priority = priority;
+    }
 }
